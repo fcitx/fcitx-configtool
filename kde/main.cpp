a0f678018356519ded70f24a12612277a2551850
csslayer <wengxt@gmail.com>
2011-08-15 22:20:19 +0800

    seperate kde part to independent project.

--- kde/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <KGenericFactory>
-#include <KPluginFactory>
-
-#include "Module.h"
-
-K_PLUGIN_FACTORY(KcmFcitxFactory,
-                 registerPlugin<Module>();)
-K_EXPORT_PLUGIN(KcmFcitxFactory("kcm_fcitx"))
