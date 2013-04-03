0f6778461bc6b7895a43b27265095aa7088d049f
Yichao Yu <yyc1992@gmail.com>
2013-04-03 09:11:12 -0400

    [cmake, i18n] remove i18n_dummy.c, using xgettext to handle gtk ui file.

--- gtk3/i18n_dummy.c
+++
@@ -1,4 +0,0 @@
-// we don't want to have intltool here
-N_("The first input method will be inactive state. Usually you need to put "
-"<b>Keyboard</b> or <b>Keyboard - <i>layout name</i></b> in the first place.");
-N_("Only Show Current Language");
