21a7cc33477268ef9c0d46f2db05487864fd41e6
chenshijie <chenshijie@uniontech.com>
2021-08-27 14:21:55 +0800

    Added configuration variables, additional input method properties interface has been opened
    Sogou, Xunfei, Huayu, Chen Qiao, have already supported
    
    http://www.znwb.com/linuxxz.html

--- gtk3/im_config_dialog.c
+++ gtk3/im_config_dialog.c
@@ -111,6 +111,37 @@ layout_foreach_cb(gpointer data, gpointer user_data)
     }
 }
 
+//If is third part im. Use their own config-UI
+int fcitx_im_config_thirdpart(FcitxConfigFileDesc *cdesc)
+{
+    char* comd = NULL;
+    char* para = NULL;
+    HASH_FOREACH(groupdesc, cdesc->groupsDesc, FcitxConfigGroupDesc) {
+        HASH_FOREACH(optiondesc, groupdesc->optionsDesc, FcitxConfigOptionDesc) {
+            if (optiondesc->desc && strlen(optiondesc->desc) != 0) {
+                if (0 == strcmp(optiondesc->optionName, "Setting") && 10 == optiondesc->type) {
+                    //printf("%s:%d\tDONE! [%s]\n", __FILE__, __LINE__, optiondesc->rawDefaultValue);
+                    comd = optiondesc->rawDefaultValue;
+                }
+                if (0 == strcmp(optiondesc->optionName, "Parameter") && 2 == optiondesc->type) {
+                    printf("%s:%d\tDONE! [%s]\n", __FILE__, __LINE__, optiondesc->rawDefaultValue);
+                    para = optiondesc->rawDefaultValue;
+                    if((NULL != comd) && (0 == strcmp("",para))){
+                        char*args[] = {comd,NULL};
+                        fcitx_utils_start_process(args);
+                        return 1;
+                    } else if((NULL != comd)&&(NULL != para)) {
+                        printf("%s:%d\tDONE! [%s][%s]\n", __FILE__, __LINE__, comd , para);
+                        char*args[] = {comd,para,NULL};
+                        fcitx_utils_start_process(args);
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
 
 GtkWidget* fcitx_im_config_dialog_new(GtkWindow* parent, FcitxAddon* addon, gchar* imname)
 {
@@ -137,6 +168,10 @@ GtkWidget* fcitx_im_config_dialog_new(GtkWindow* parent, FcitxAddon* addon, gcha
         cfdesc = get_config_desc(config_desc_name);
         g_free(config_desc_name);
         configurable = (gboolean)(cfdesc != NULL || strlen(addon->subconfig) != 0);
+    }
+
+    if (NULL != cfdesc && fcitx_im_config_thirdpart(cfdesc)) {
+        return NULL;
     }
 
     do {
